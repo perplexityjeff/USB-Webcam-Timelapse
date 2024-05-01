aa330c7865a56a0075ffc9b5dd3b838ecd3f2870
Jeffrey Jansma <jeffreyjansma@proton.me>
2024-05-01 12:23:21 +0200

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,72 +1,15 @@
 #!/usr/bin/env python
 
-#
-#  ### Program for making timelapse out of webcam images ###
-#
-#  usage: $ ./webcam.py 3 2d webcamImages http://webcam.com/current.jpg
-#
-#  3: interval between images in seconds
-#  2d: How long to keep recording (X d/h/m)
-#  webcamImages: Name of the folder to save images. Will be created in getcwd()
-#  http://webcam.com/current.jpg: URL of the current image
-#
-#  After the specified time has elapsed, a video will be created at 60 fps.
-#  If you want to make another video at another framerate, use video.py.
-#
-#  Some webcam urls to try:
-#
-#  http://hgwwebcam.dyndns.org:5000/axis-cgi/jpg/image.cgi   Sailboats, ZH, CH
-#  http://80.75.114.18/axis-cgi/jpg/image.cgi?clock=1&text=0 KVA Horgen -> ZH
-#  http://80.75.114.19/axis-cgi/jpg/image.cgi?clock=1&text=0 KVA Horgen -> Rappi
-#  http://webcam.bodmer-chur.ch:8080/axis-cgi/jpg/image.cgi  Chur, CH
-#  http://bautzen.redirectme.net/axis-cgi/jpg/image.cgi      German Airport
-#  http://194.218.96.90/axis-cgi/jpg/image.cgi               Swedish Village
-#  http://axis-lyss.axiscam.net:8080/axis-cgi/jpg/image.cgi  Geneva Roundabout
-#  http://80.254.173.253:8080/axis-cgi/jpg/image.cgi         Interlaken, CH
-#  http://146.186.123.229/axis-cgi/jpg/image.cgi             Penn State
-#  http://77.106.162.110:8081/axis-cgi/jpg/image.cgi         Ski resort
-#  http://webcam01.bigskyresort.com/axis-cgi/jpg/image.cgi   Ski resort
-#  http://wc1.bluffton.edu/axis-cgi/jpg/image.cgi?resolution=2048x1536
-#  http://62.202.23.123/record/current.jpg                   Lake Zurich
-#  http://62.202.21.101/record/current.jpg                   Hotel somewhere
-#  http://www.hoteltell.ch/Webcam/current.jpg                Schwyz, CH (only every min)
-#  http://www.altenburg.eu/WebCam1/axis-cgi/jpg/image.cgi    Altenburg, CH
-#  http://212.254.182.126/axis-cgi/jpg/image.cgi             Niesen, CH
-#  http://213.213.183.83/SnapshotJPEG?Resolution=640x480     Bern, CH
-#  http://213.221.150.136/axis-cgi/jpg/image.cgi             Monthey, CH
-#  http://hogakusten.mine.nu/axis-cgi/jpg/image.cgi          Bridge in Sweden
-#  https://s3-eu-west-1.amazonaws.com/felsenegg-cam/cam0.jpg Restaurant Felsenegg
-#  http://46.140.114.222:8080/axis-cgi/jpg/image.cgi         Balkon
-#  http://178.198.19.229/axis-cgi/jpg/image.cgi              UAV Lab
-#  http://178.198.70.22/axis-cgi/jpg/image.cgi               Camping
-#  http://212.243.94.2/axis-cgi/jpg/image.cgi                Honegg bei Buochs
-#  http://178.199.206.91/axis-cgi/jpg/image.cgi              Amateur radio shop
-#  http://46.150.196.132/cgi-bin/jpg/image.cgi               Roundabout
-#  http://axis.meisal.com/axis-cgi/jpg/image.cgi             Fjellgrend, Norway
-#  http://posta.mukolin.cz/axis-cgi/jpg/image.cgi            Czech (?) town square
-#
-#  ... http://members.upc.nl/a.horlings/doc-google.html
-#
-#  UPDATE
-#  ======
-#  A simpler way to get images is the following:
-#  while true; do echo $URL; sleep 2; done | xargs -n1 -P8 -I{} bash -c 'date +%s; wget {} -O $(date +%s).jpeg -a /dev/null;'
-# Which has the advantage that if loading an image takes longer than the planned time between
-# downloads, xargs makes the next request while the previous download is still running.
-# (It's up to the server if it can anwer requests that fast)
-#
-
-from moviepy.editor import *
-import requests
 import time
 import sys
 import os
+import cv2 
+import datetime
 
 # Process arguments
 period = float(sys.argv[1])
 folder = str(sys.argv[3])
-url    = str(sys.argv[4])
-
+cam_port = str(sys.argv[4])
 duration = int(sys.argv[2][:-1])
 
 # Convert duration of recording to seconds
@@ -81,21 +24,28 @@ elif sys.argv[2].endswith("s"):
 else:
     print("Invalid argument: sys.argv[2] must be an integer + 'd', 'h', 'm' or 's'")
 
-
 # Directory stuff
 homeDirectory = os.getcwd()
 absPath = homeDirectory + '/' + folder
+
+# Webcam stuff
+vid = cv2.VideoCapture(cam_port)
+vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+width = 1920
+height = 1080
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
 if not os.path.exists(folder):
     os.makedirs(folder)
 
 # Downloader function
-def downloadImage(url, dir, name):
-    f = open(os.path.join(dir, name), 'wb')
-    f.write(requests.get(url).content)
-    f.close()
-
-    print('Saved image: ' + folder + '/' + name)
+def downloadImage(dir, name):
+    # Saving of webcam image (Added by Jeff)
+    result, image = vid.read() 
+    if result: 
+        cv2.imwrite(os.path.join(dir, name), image) 
+        print('Saved image: ' + folder + '/' + name)
 
 # Timing
 nextTime = time.time()
@@ -108,17 +58,16 @@ while currentTime < endTime:
     if currentTime >= nextTime:
         nextTime += period
 
-        timeStr = '{0}.jpg'.format(int(currentTime))
-        downloadImage(url, folder, timeStr)
+        named_tuple = time.localtime()
+        time_string = time.strftime("%Y-%m-%d-%H%M%S", named_tuple)
+        timeStr = '{0}.jpg'.format(time_string)
+
+        now = datetime.datetime.now()
+        if now.hour >= 7 and now.hour < 18:
+            downloadImage(folder, timeStr)
+        else:
+            print("Skipped image")
 
     # Sleep 0.1 seconds to make sure nothing is missed
     # Also you can set the interval to something like 0.3 secs
     time.sleep(0.1)
-
-# Make the video - 60fps default
-outputName = folder + '.mp4'
-names = [folder + '/' + name for name in os.listdir(folder) if name.endswith(".jpg")]
-names.sort()
-
-clip = ImageSequenceClip(names, fps = 60)
-clip.write_videofile(outputName, fps = 60)
