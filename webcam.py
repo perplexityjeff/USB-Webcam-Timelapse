8ac308b8dc86da871db9ec9eda7207ab5ec2bb02
Jeffrey Jansma <jeffreyjansma@proton.me>
2024-05-02 11:32:53 +0200

    Camera resolution format bug fixed and small restructure
    Camera resolution format bug fixed and small restructure with addition of credits to the original author.

--- webcam.py
+++ webcam.py
@@ -10,8 +10,8 @@ import datetime
 period = float(sys.argv[1])
 folder = str(sys.argv[3])
 cam_port = str(sys.argv[4])
-cam_width = str(sys.argv[5])
-cam_height = str(sys.argv[6])
+cam_width = int(sys.argv[5])
+cam_height = int(sys.argv[6])
 duration = int(sys.argv[2][:-1])
 
 # Convert duration of recording to seconds
@@ -26,21 +26,32 @@ elif sys.argv[2].endswith("s"):
 else:
     print("Invalid argument: sys.argv[2] must be an integer + 'd', 'h', 'm' or 's'")
 
+
+print(" _   _ ____  ____     __        __   _                             _____ _                _                       ")
+print("| | | / ___|| __ )    \ \      / /__| |__   ___ __ _ _ __ ___     |_   _(_)_ __ ___   ___| | __ _ _ __  ___  ___  ")
+print("| | | \___ \|  _ \ ____\ \ /\ / / _ \ '_ \ / __/ _` | '_ ` _ \ _____| | | | '_ ` _ \ / _ \ |/ _` | '_ \/ __|/ _ \ ")
+print("| |_| |___) | |_) |_____\ V  V /  __/ |_) | (_| (_| | | | | | |_____| | | | | | | | |  __/ | (_| | |_) \__ \  __/ ")
+print(" \___/|____/|____/       \_/\_/ \___|_.__/ \___\__,_|_| |_| |_|     |_| |_|_| |_| |_|\___|_|\__,_| .__/|___/\___| ")           
+print("                                                                                                 |_|              ")
+print("")
+print("A script based on the work of mbjd's WebcamTimelapse on GitHub, modified to work with USB camera's by perplexityjeff")
+print("https://github.com/perplexityjeff/USB-Webcam-Timelapse")
+print("")
+
 # Directory stuff
+print("Setting directory parameters")
 homeDirectory = os.getcwd()
 absPath = homeDirectory + '/' + folder
-
-# Webcam stuff
-vid = cv2.VideoCapture(cam_port)
-
-#forces MJPG, this will be an option in the future maybe
-#vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-
-vid.set(cv2.CAP_PROP_FRAME_WIDTH, cam_width)
-vid.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_height)
-
 if not os.path.exists(folder):
     os.makedirs(folder)
+
+# Webcam stuff
+print("Setting camera parameters")
+vid = cv2.VideoCapture(cam_port)
+#forces MJPG, this will be an option in the future maybe
+#vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, cam_width)
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_height)
 
 # Saving of webcam image
 def getCameraImage(dir, name):   
@@ -54,6 +65,7 @@ nextTime = time.time()
 currentTime = time.time()
 
 # Get the images for as long as specified
+print("Starting the timelapse")
 while currentTime < endTime:
     currentTime = time.time()
 
